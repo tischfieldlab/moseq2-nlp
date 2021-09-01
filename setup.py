6ebf2c0662342a2019ef84b7d1032aeca2dc2d1b
quantumdot <quantumdot@users.noreply.github.com>
2021-09-01 11:00:24 -0400

    cleanup repo - remove errounously committed code - create packaging structure - update setup.py - move notebooks to their own directory

--- setup.py
+++ setup.py
@@ -1,28 +1,13 @@
 from setuptools import setup, find_packages
-import subprocess
-import sys
-
-
-def install(package):
-    subprocess.call([sys.executable, "-m", "pip", "install", package])
-
-
-try:
-    import cv2
-except ImportError:
-    install('opencv-python')
 
 
 setup(
-    name='moseq2-viz',
-    author='Jeff Markowitz',
-    description='To boldly go where no mouse has gone before',
-    version='0.1.3',
+    name='moseq2-nlp',
+    description='Interrogating Moseq data using a NLP-based approach',
+    version='0.1.0',
     packages=find_packages(),
-    platforms=['mac', 'unix'],
-    install_requires=['tqdm', 'matplotlib', 'click', 'dtaidistance', 'sklearn',
-                      'ruamel.yaml>=0.15.0', 'seaborn', 'psutil',
-                      'pandas', 'networkx', 'numpy'],
+    #platforms=['mac', 'unix'],
+    install_requires=[],
     python_requires='>=3.6',
-    entry_points={'console_scripts': ['moseq2-viz = moseq2_viz.cli:cli']}
+    #entry_points={'console_scripts': ['moseq2-viz = moseq2_viz.cli:cli']}
 )
