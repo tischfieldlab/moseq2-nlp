d94a3c5b2208dc547d32ea252fdff52c1e134a4e
quantumdot <quantumdot@users.noreply.github.com>
2021-09-08 12:32:15 -0400

    add missing packages to setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,16 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     #platforms=['mac', 'unix'],
-    install_requires=['numpy==1.19.2', 'tqdm==4.36.1', 'gensim==4.0.1','ConfigArgParse==1.5.2','scikit_learn==0.24.2'],
     python_requires='>=3.6',
+    install_requires=[
+        'numpy==1.19.2',
+        'tqdm==4.36.1',
+        'gensim==4.0.1',
+        'python-Levenshtein',
+        'ConfigArgParse==1.5.2',
+        'scikit_learn==0.24.2'
+        'h5py',
+        'moseq2-viz @ git+https://github.com/tischfieldlab/moseq2-viz.git@master'
+    ],
     #entry_points={'console_scripts': ['moseq2-viz = moseq2_viz.cli:cli']}
 )
