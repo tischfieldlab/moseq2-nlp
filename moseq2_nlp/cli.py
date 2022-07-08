27995aff3fac5aae5740c799063b0b0f40c7fb36
quantumdot <quantumdot@users.noreply.github.com>
2022-07-08 14:38:21 -0400

    fix slurm parameters for grid search, more generalizable

--- moseq2_nlp/cli.py
+++ moseq2_nlp/cli.py
@@ -3,19 +3,20 @@ import sys
 from functools import partial
 
 import click
-from numpy.random import randint, choice
+from click_option_group import optgroup
+from numpy.random import choice, randint
+from tqdm import tqdm
 
 import moseq2_nlp.train as trainer
-from moseq2_nlp.gridsearch import (find_gridsearch_results, generate_grid_search_worker_params,
+from moseq2_nlp.data import get_raw_data, make_phrases_dataset
+from moseq2_nlp.gridsearch import (find_gridsearch_results,
+                                   generate_grid_search_worker_params,
                                    get_gridsearch_default_scans,
                                    wrap_command_with_local,
                                    wrap_command_with_slurm, write_jobs)
 from moseq2_nlp.utils import (IntChoice, command_with_config, ensure_dir,
                               get_command_defaults, write_yaml)
-
-from moseq2_nlp.data import get_raw_data, make_phrases_dataset
 from moseq2_nlp.visualize import make_wordcloud
-from tqdm import tqdm
 
 # Here we will monkey-patch click Option __init__
 # in order to force showing default values for all options
@@ -33,6 +34,7 @@ click.core.Option.__init__ = new_init # type: ignore
 @click.version_option()
 def cli():
     pass
+
 
 @cli.command(name='train', cls=command_with_config('config_file'), help='train a classifier')
 @click.option('--name', type=str)
@@ -64,12 +66,14 @@ def train(name, save_dir, model_path, index_path, representation, classifier, em
     trainer.train(name, save_dir, model_path, index_path, representation, classifier, emissions, custom_groupings, num_syllables, num_transitions, min_count, dm, embedding_dim, embedding_window,
           embedding_epochs, bad_syllables, scoring, k, penalty, num_c, kernel, seed)
 
+
 @cli.command(name="generate-train-config", help="Generates a configuration file that holds editable options for training parameters.")
 @click.option('--output-file', '-o', type=click.Path(), default='train-config.yaml')
 def generate_train_config(output_file):
     output_file = os.path.abspath(output_file)
     write_yaml(output_file, get_command_defaults(train))
     print(f'Successfully generated train config file at "{output_file}".')
+
 
 @cli.command(name="make-random-documents", help="Splits frames or emissions into sentences of random lengths and keeps track of which sentences belong to what class.")
 @click.option('--model-path', type=str, default='/cs/usr/ricci/data/abraira/robust_septrans_model_20min_1000.p')
@@ -115,6 +119,7 @@ def make_random_documents(model_path, index_path, splits, min_length, max_length
     
     print(f'Successfully generated random train/test documents at "{save_dir}".')
 
+
 @cli.command(name='make-phrases', help='finds and saves compound modules')
 @click.argument('model-path', type=click.Path(exists=True))
 @click.argument('index-path', type=click.Path(exists=True))
@@ -132,29 +137,39 @@ def make_phrases(model_path, index_path, save_path, wordcloud_path, threshes, n,
         print('Making word cloud')
         make_wordcloud(save_path, wordcloud_path, max_plot=max_plot)
 
+
 @cli.command(name='grid-search', help='grid search hyperparameters')
 @click.argument("scan_file", type=click.Path(exists=True))
-@click.option('--env', type=str, default='moseq2-nlp', help="Environment in which to run jobs")
 @click.option('--save-dir', type=click.Path(), default=os.path.join(os.getcwd(), 'worker-configs'), help="Directory to save worker configurations")
 @click.option('--cluster-type', default='local', type=click.Choice(['local', 'slurm']))
-@click.option('--slurm-ncpus', type=int, default=1, help="Number of CPUs per job. Only for SLURM")
-@click.option('--slurm-memory', type=str, default="2GB", help="Amount of memory per job. Only for SLURM")
-@click.option('--slurm-wall-time', type=str, default='6:00:00', help="Max wall time per job. Only for SLURM")
-@click.option('--slurm-killable', is_flag=True)
-def grid_search(scan_file, env, save_dir, cluster_type, slurm_ncpus, slurm_memory, slurm_wall_time, slurm_killable):
+@optgroup.group('SLURM Scheduler Options', help="The following parameters affect how SLURM jobs are requested, ignored unless --cluster-type=slrum")
+@optgroup.option('--slurm-partition', type=str, default='main', help="Partition on which to run jobs. Only for SLURM")
+@optgroup.option('--slurm-ncpus', type=int, default=1, help="Number of CPUs per job. Only for SLURM")
+@optgroup.option('--slurm-memory', type=str, default="2GB", help="Amount of memory per job. Only for SLURM")
+@optgroup.option('--slurm-wall-time', type=str, default='6:00:00', help="Max wall time per job. Only for SLURM")
+@optgroup.option('--slurm-preamble', type=str, default='', help="Extra commands to run prior to executing job. Useful for activating an environment, if needed")
+@optgroup.option('--slurm-extra', type=str, default='', help="Extra parameters to pass to surm.")
+def grid_search(scan_file, save_dir, cluster_type, slurm_partition, slurm_ncpus, slurm_memory, slurm_wall_time, slurm_preamble, slurm_extra):
 
     worker_dicts = generate_grid_search_worker_params(scan_file)
 
     if cluster_type == 'local':
         cluster_wrap = wrap_command_with_local
     elif cluster_type == 'slurm':
-        cluster_wrap = partial(wrap_command_with_slurm, env=env, ncpus=slurm_ncpus, memory=slurm_memory, wall_time=slurm_wall_time, killable=slurm_killable)
+        cluster_wrap = partial(wrap_command_with_slurm,
+                               preamble=slurm_preamble,
+                               partition=slurm_partition,
+                               ncpus=slurm_ncpus,
+                               memory=slurm_memory,
+                               wall_time=slurm_wall_time,
+                               extra_params=slurm_extra)
     else:
         raise ValueError(f'Unsupported cluster-type {cluster_type}')
 
     save_dir = ensure_dir(save_dir)
     write_jobs(worker_dicts, cluster_wrap, save_dir)
     sys.stderr.write(f'{len(worker_dicts)} jobs written to {save_dir}\n')
+
 
 @cli.command(name="generate-gridsearch-config", help="Generates a configuration file that holds editable options for gridsearching hyperparameters.")
 @click.option('--output-file', '-o', type=click.Path(), default='gridsearch-config.yaml')
@@ -169,6 +184,7 @@ def generate_gridsearch_config(output_file):
     write_yaml(output_file, params)
     print(f'Successfully generated gridsearch config file at "{output_file}".')
 
+
 @cli.command(name="aggregate-gridsearch-results", help="Aggregate Gridsearch results.")
 @click.argument("results-directory", type=click.Path(exists=True))
 @click.option("--best-key", type=str, default='best_accuracy')
@@ -179,6 +195,7 @@ def aggregate_gridsearch_results(results_directory, best_key):
 
     print('Best model:')
     print(results.iloc[0])
+
 
 if __name__ == '__main__':
     cli()
